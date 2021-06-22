2bc60ba2dddd47ce7bf5a04ca871e6dd28cb9864
Artem Gorokhovskii <a84277755@gmail.com>
2021-06-23 00:01:04 +0300

    draft

---
+++ ASP-Test-3/Config/TokenConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthTest.Config
+{
+    public class TokenConfig
+    {
+        static TokenConfig()
+        {
+            string symmetricKey = File.ReadAllText(Path.Combine(
+                Directory.GetParent(Environment.CurrentDirectory).ToString(),
+                "Secret",
+                "TokenSecret.txt"
+            ));
+            SYMMETRIC_KEY = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(symmetricKey));
+        }
+
+        public static string ISSUER = "MyAuthServer";
+        public static string AUDIENCE = "MyAuthClient";
+        public static int LIFETIME = 1;
+        public static SymmetricSecurityKey SYMMETRIC_KEY = null;
+    }
+}
---
+++ ASP-Test-3/Service/AuthService.cs
@@ -0,0 +1,47 @@
+﻿using AuthTest.Config;
+using AuthTest.Entity;
+using AuthTest.Exceptions;
+using AuthTest.Repository;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthTest.Service
+{
+    public class AuthService : IAuthService
+    {
+        public string Auth(string login, string password)
+        {
+            UserRepository userRepository = new UserRepository();
+            User user = userRepository.GetUser(login);
+            if (user == null)
+            {
+                throw new BadRequestException("Login or password are incorrect");
+            }
+            
+            if (user.Password != password)
+            {
+                throw new BadRequestException("Login or password are incorrect");
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: TokenConfig.ISSUER,
+                    audience: TokenConfig.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(TokenConfig.LIFETIME)),
+                    signingCredentials: new SigningCredentials(TokenConfig.SYMMETRIC_KEY, SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+        private Exception UnauthorizedException()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
